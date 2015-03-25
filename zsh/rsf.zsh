30d3701eac13cd32793882301500191b8235a103
Christopher Chalcraft <chris.chalcraft@gmail.com>
2015-03-24 19:11:48 -0700

    added Madagascar exports in rsf.zsh

---
+++ zsh/rsf.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+# ============ Exports for Madagascar=================
+
+if [ -d /usr/local/rsf ]
+   RSFROOT="/usr/local/rsf/" # If we have madagascar installed, add it
+   if [ $?PYTHONPATH ] then
+       PYTHONPATH=$PYTHONPATH:/lib
+   else
+       PYTHONPATH=$PYTHONPATH:$RSFROOT/lib
+   fi
+   PATH="$PATH:/bin"
+   MANPATH="$RSFROOT/share/man:`manpath`"
+   LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$RSFROOT/lib"
+fi
+
+
