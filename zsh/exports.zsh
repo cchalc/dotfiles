70ef34e2a2a57516f403cfe13b85849378a9c09a
Christopher Chalcraft <chris.chalcraft@gmail.com>
2016-12-19 14:17:54 -0700

    upgraded to anaconda3

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -7,4 +7,4 @@ export ARCHFLAGS="-arch x86_64" # Stock Arch Linux
 test -d "/usr/bin/vendor_perl" && PATH="$PATH:/usr/bin/vendor_perl" # If Perl is found, add to PATH
 test -d "$HOME/texmf" && TEXMFHOME="$HOME/texmf" # If we have a local texmf, add it
 test -d "$HOME/bin" && PATH="$PATH:$HOME/bin" # If we have a local bin, add it
-test -d "/home/cchalc/Programs/anaconda/bin" && PATH="$PATH:/home/cchalc/Programs/anaconda/bin" # If we have anaconda installed
+test -d "/home/cchalc/Programs/anaconda3/bin" && PATH="$PATH:/home/cchalc/Programs/anaconda3/bin" # If we have anaconda installed
