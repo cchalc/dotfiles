f7d3310aee502af9d92a8dc20608656c9405c6b6
Christopher Chalcraft <chris.chalcraft@gmail.com>
2018-03-02 13:42:14 -0700

    updated haskell support

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -10,3 +10,8 @@ test -d "$HOME/bin" && PATH="$PATH:$HOME/bin" # If we have a local bin, add it
 test -d "/home/cchalc/Programs/anaconda/bin" && PATH="$PATH:/home/cchalc/Programs/anaconda/bin" # If we have anaconda installed
 test -d "$HOME/go" && GOPATH="$HOME/go" # set the GOPATH environment variable
 test -d "$HOME/go/bin" && PATH="$PATH:$HOME/go/bin" # If we have go installed, add it
+test -d "$HOME/anaconda3" && PATH="$PATH:$HOME/anaconda3/bin"
+test -d "$HOME/.cabal" && PATH="$PATH:$HOME/.cabal/bin"
+#test -d "$HOME/.rvm/scripts/rvm" && PATH="$PATH:$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+#test -d "$HOME/.rvm/scripts/rvm" && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+#test -d "/usr/bin/ruby" && PATH="$PATH:$(ruby -e 'print Gem.user_dir')/bin" # If we have go installed, add it
