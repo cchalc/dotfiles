9c6b5194319f8b465e9c7fbc13b351abf5b89f85
Christopher Chalcraft <chris.chalcraft@gmail.com>
2019-11-24 00:08:27 -0700

    Added files to install and exports

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -12,4 +12,3 @@ test -d "$HOME/go/bin" && PATH="$PATH:$HOME/go/bin" # If we have go installed, a
 test -d "$HOME/anaconda3" && PATH="$PATH:$HOME/anaconda3/bin"
 test -d "$HOME/.cabal" && PATH="$PATH:$HOME/.cabal/bin"
 #test -d "/usr/bin/ruby" && PATH="$PATH:$(ruby -e 'print Gem.user_dir')/bin" # If we have go installed, add it
-#test -f "$HOME/.local/bin/virtualenvwrapper.sh" && PATH="$PATH:$HOME/" # .local/bin, add it
