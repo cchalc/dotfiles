0af5bb33edd766b14a1914bda4826c261dcfc01e
Christopher Chalcraft <chris.chalcraft@gmail.com>
2019-11-23 23:15:47 -0700

    data science source

--- other/exports_local.zsh
+++ other/exports_local.zsh
@@ -4,3 +4,9 @@
 
 test -d "/home/cchalc/Projects/Crypto/fabric-samples" && export HLPATH="/home/cchalc/Projects/Crypto/fabric-samples/bin" # If we have hyperledger installed
 test -d "$HLPATH" && PATH="$PATH:$HLPATH" # If we have go installed, add it
+
+# Broken
+#test -d $HOME/.local/bin && export PATH="$PATH:$HOME/.local/bin" && source /home/cchalc/.local/bin/virtualenvwrapper.sh
+
+export EMAIL_HOST_USER="chris.chalcraft@gmail.com"
+export EMAIL_HOST_PASSWORD="Quinn314*!"
